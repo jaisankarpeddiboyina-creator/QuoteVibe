a7b121e9f2e10f8499afff217d6ac6f85abd7a45
Jai Sankar Peddiboyina <jaisankarpeddiboyina@gmail.com>
2026-02-22 13:13:54 +0530

    QuoteVibe v1.0 - icons, policy pages, performance, service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+﻿const CACHE = 'quotevibe-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
